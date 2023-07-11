dcca6bac74cfaf685db06d827693c29fea30f115
lucio <lucio@LAPTOP-LTNU73BF>
2023-07-11 14:59:27 -0300

    Agregar archivos de proyecto.

---
+++ asd.consola/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace asd.consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
